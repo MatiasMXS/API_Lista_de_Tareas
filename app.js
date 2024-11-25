1a6337e41dab9371c3990fbde6b87f7714a9bc95
MatiasMXS <matifmorenoxd@gmail.com>
2024-11-25 16:50:54 -0300

    deploy vercel

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const express = require('express');
+console.log("Aplicacion iniciando...");
+
+require("dotenv").config(); // importamos en dontev para poder usar las variables de entorno
+const express = require("express");
 const app = express();
-const dbconnect = require('./config/db'); // Importar la conexión a la base de datos
-const tareasRoutes = require('./routes/tareas'); // Importar las rutas de libros
+const dbconnect = require("./config/db"); // Importar la conexión a la base de datos
+const tareasRoutes = require("./routes/tareas"); // Importar las rutas de libros
 
 app.use(express.json()); // Middleware para interpretar JSON
 
 // Usar las rutas de libros
-app.use(tareasRoutes);
+app.use("/api", tareasRoutes);
 
 // Probar la conexión a la base de datos y arrancar el servidor
-dbconnect().then(() => {
-    app.listen(3000, () => {
-        console.log('El servidor está corriendo en el puerto 3000');
-    });
-}).catch(err => {
-    console.error('No se pudo iniciar el servidor debido a un error en la base de datos');
-});
-
+dbconnect()
+  .then(() => {
+    console.log("El servidor está corriendo en el puerto 3000");
+  })
+  .catch((err) => {
+    console.error(
+      "No se pudo iniciar el servidor debido a un error en la base de datos"
+    );
+  });
+module.exports = app; //delegamoas la aplicacion para poder delegarle el control alVercel
